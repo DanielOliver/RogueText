17c1d0bc12b3bd2f2c7dbaf2e4a5eb886df64a5c
Daniel Oliver <11494375+DanielOliver@users.noreply.github.com>
2017-11-05 16:48:54 -0500

    Add super simple highest level tokenizer.

--- RogueText.Examples/Program.fs
+++ RogueText.Examples/Program.fs
@@ -9,16 +9,8 @@ let main argv =
 
 
     let input = "{{ text/}} one + 99 and two{{ four}}without seven unless nine{{/}}"
-    let separators = 
-        [|  "{{/}}"
-            "{{/"
-            "/}}"
-            "{{"
-            "}}"
-        |]
-    let separatorRegex = "(" + String.Join("|", separators) + ")"
-    let regex = System.Text.RegularExpressions.Regex(separatorRegex)
-    let output = regex.Split(input) |> Array.filter (String.IsNullOrWhiteSpace >> not)
+
+    let output = RogueText.Tokenizer.TokenizeTags input
     printfn "%A" output
 
     System.Console.ReadLine() |> ignore
---
+++ RogueText/Tokenizer.fs
@@ -0,0 +1,38 @@
+﻿module RogueText.Tokenizer
+
+open System
+
+let private tagSeparators = 
+    [|  "{{/}}"
+        "/}}"
+        "{{"
+        "}}"
+    |]
+let private tagSeparatorRegexString = "(" + String.Join("|", tagSeparators) + ")"
+let private tagSeparatorRegex = Text.RegularExpressions.Regex(tagSeparatorRegexString)
+
+[<RequireQualifiedAccess>]
+type TagToken =
+    | CloseExpression
+    | OpenTagClose
+    | OpenTagStart
+    | OpenTagEnd
+    | Text of string
+    | Whitespace
+
+    static member Identify text =
+        match text with
+        | "{{/}}" -> TagToken.CloseExpression
+        | "/}}" -> TagToken.OpenTagClose
+        | "{{" -> TagToken.OpenTagStart
+        | "}}" -> TagToken.OpenTagEnd
+        | x when String.IsNullOrWhiteSpace(x) -> TagToken.Whitespace
+        | _ -> TagToken.Text text
+
+let TokenizeTags text = 
+    text 
+    |> tagSeparatorRegex.Split 
+    |> Array.filter(fun t -> t.Length > 0) 
+    |> Array.map TagToken.Identify
+
+
