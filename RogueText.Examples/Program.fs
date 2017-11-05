7e5cafb2568e225ee4b6c862a802d98a0de3d2b7
Daniel Oliver <danieloliversoftware.com>
2017-11-05 15:43:22 -0500

    playing around with regex.

---
+++ RogueText.Examples/Program.fs
@@ -0,0 +1,25 @@
+﻿open System
+open System.Text.RegularExpressions
+
+// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+[<EntryPoint>]
+let main argv = 
+
+
+    let input = "{{ text/}} one + 99 and two{{ four}}without seven unless nine{{/}}"
+    let separators = 
+        [|  "{{/}}"
+            "{{/"
+            "/}}"
+            "{{"
+            "}}"
+        |]
+    let separatorRegex = "(" + String.Join("|", separators) + ")"
+    let regex = System.Text.RegularExpressions.Regex(separatorRegex)
+    let output = regex.Split(input) |> Array.filter (String.IsNullOrWhiteSpace >> not)
+    printfn "%A" output
+
+    System.Console.ReadLine() |> ignore
+    0 // return an integer exit code
