4259352d1bae686d7e20960cc24b824033291e5d
Daniel Oliver <11494375+DanielOliver@users.noreply.github.com>
2017-11-12 17:50:58 -0500

    First step to type provider.

--- RogueText.Examples/Program.fs
+++ RogueText.Examples/Program.fs
@@ -1,4 +1,7 @@
 ﻿
+type Example = RogueText.Print<"one">
+
+
 [<EntryPoint>]
 let main argv = 
     "{ color='primaryColor' pluralIf='@variable1'}{ number='variable1' capitalize bold>} { word='mouse'>} { word='has'>} appeared. Mice are fierce fighters and deadly if swarming.{>} Mice are {@ something? }"
@@ -9,7 +12,8 @@ let main argv =
             |> Seq.iter (printfn "%A")
 
         | Error err -> err |> printfn "%A")
-
+        
+    Example.show |> printfn "%s"
     System.Console.ReadLine() |> ignore
     0 // return an integer exit code
     
---
+++ RogueText/TaggedTextProvider.fs
@@ -0,0 +1,47 @@
+﻿namespace RogueText 
+
+
+open System
+open System.IO
+open Microsoft.FSharp.Core.CompilerServices
+open ProviderImplementation.ProvidedTypes
+open System.Reflection
+
+
+module private ProviderTests =
+    open Microsoft.FSharp.Quotations
+    
+    let toProperty text =
+        let invokeCode =
+            Expr.Value text
+        let invokeType = invokeCode.Type
+        ProvidedProperty("show", invokeType, isStatic = true, getterCode = fun _ -> invokeCode)
+
+// --------------------------------------------------------------------------------------
+
+[<TypeProvider>]
+type public TaggedTextProvider(cfg:TypeProviderConfig) as this =
+    inherit TypeProviderForNamespaces(cfg)
+    
+    let ns = "RogueText"
+    let asm = Assembly.GetExecutingAssembly()
+
+    let tPrintProvider = ProvidedTypeDefinition(asm, ns, "Print", Some(typeof<obj>))
+
+    let parameters = [ProvidedStaticParameter("FormatString", typeof<string>)]
+
+    do tPrintProvider.DefineStaticParameters(parameters, fun typeName args ->
+        let formatString = args.[0] :?> string
+
+        let provider = ProvidedTypeDefinition(asm, ns, typeName, Some(typeof<obj>))
+        provider.HideObjectMethods <- true
+        formatString |> ProviderTests.toProperty |> provider.AddMember
+
+        provider
+        )
+    
+    do
+        this.AddNamespace(ns, [tPrintProvider])
+
+[<assembly:TypeProviderAssembly>]
+do ()
